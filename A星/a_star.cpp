66390780f88258d9964272c09a4fd491ba42c930
“amuge1997” <amuge1997z@qq.com>
2023-10-14 21:26:16 +0800

    算法

--- A星/a_star.cpp
+++ A星/a_star.cpp
@@ -157,6 +157,12 @@ void print_grid_list_pos(grid_list* list){
 }
 
 
+struct pair{
+    int r;
+    int c;
+};
+
+
 // 探索附近网格
 void find_near_grid(
     grid** the_grid_map, 
@@ -173,42 +179,43 @@ void find_near_grid(
     x_low = 0;
     x_up = cols;
 
-    for(int row=-1;row<2;row++){
-        for(int col=-1;col<2;col++){
-            if(row == 0 and col == 0) continue;
+    pair pa[4] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-            position near_position = {this_grid->pos.y + row, this_grid->pos.x + col};
-            if(near_position.x < x_low or near_position.x >= x_up){}
-            else if (near_position.y < y_low or near_position.y >= y_up){}
-            else if (the_grid_map[near_position.y][near_position.x].map_type == BLOCK){}
-            else{
-                // 计算附近网格g和h
-                grid* near_grid = &the_grid_map[near_position.y][near_position.x];
-                float h = distance(target_position, near_grid->pos, 'h');
-                float d = distance(this_grid->pos, near_grid->pos, 'o');
-                float g = d + this_grid->g;
-                if(!is_in_list(close_list, near_grid)){
-                    if(!is_in_list(open_list, near_grid)){
-                        // 未出现在待探索列表中
-                        float f = h + g;
-                        near_grid->from_grid = this_grid;
-                        near_grid->f = f;
-                        near_grid->h = h;
+    for(int i=0;i<4;i++){
+        int row = pa[i].r;
+        int col = pa[i].c;
+
+        position near_position = {this_grid->pos.y + row, this_grid->pos.x + col};
+        if(near_position.x < x_low or near_position.x >= x_up){}
+        else if (near_position.y < y_low or near_position.y >= y_up){}
+        else if (the_grid_map[near_position.y][near_position.x].map_type == BLOCK){}
+        else{
+            // 计算附近网格g和h
+            grid* near_grid = &the_grid_map[near_position.y][near_position.x];
+            float h = distance(target_position, near_grid->pos, 'h');
+            float d = distance(this_grid->pos, near_grid->pos, 'o');
+            float g = d + this_grid->g;
+            if(!is_in_list(close_list, near_grid)){
+                if(!is_in_list(open_list, near_grid)){
+                    // 未出现在待探索列表中
+                    float f = h + g;
+                    near_grid->from_grid = this_grid;
+                    near_grid->f = f;
+                    near_grid->h = h;
+                    near_grid->g = g;
+                    list_append(open_list, near_grid);
+                }else{
+                    // 已出现在待探索列表中
+                    if(near_grid->g > g){
+                        // 若从当前网格到附近网格距离更优,更新附近网格g值
                         near_grid->g = g;
-                        list_append(open_list, near_grid);
-                    }else{
-                        // 已出现在待探索列表中
-                        if(near_grid->g > g){
-                            // 若从当前网格到附近网格距离更优,更新附近网格g值
-                            near_grid->g = g;
-                            near_grid->from_grid = this_grid;
-                        }
+                        near_grid->from_grid = this_grid;
                     }
                 }
             }
-            
         }
     }
+    
 }
 
 
@@ -299,51 +306,72 @@ void show_route(grid_list* lines){
 }
 
 
+// 绘制
+void draw_result(
+    int** the_map,
+    position start_position, 
+    position target_position,
+    grid_list* lines,
+    int rows, int cols
+){
+    for(int ri=0;ri<rows;ri++){
+        for(int ci=0;ci<cols;ci++){
+            
+            bool is_draw = false;
+            if(ri==start_position.y && ci==start_position.x){
+                printf("G ");
+                is_draw = true;
+            }
+            else if (ri==target_position.y && ci==target_position.x)
+            {
+                printf("X ");
+                is_draw = true;
+            }
+
+            for(int i=0;i<lines->tail_index+1;i++){
+                if(ri == lines->array[i]->pos.y && ci == lines->array[i]->pos.x && !is_draw){
+                    // printf("● ");
+                    printf("○ ");
+                    is_draw = true;
+                    break;
+                }
+            }
+            if(!is_draw){
+                if(BLOCK == the_map[ri][ci]){
+                    printf("■ ");
+                    // printf("□ ");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+
+        }
+        print_n();
+    }
+    print_n();
+    print_n();
+
+}
+
+
 void run(){
-    // 地图构建1
-    // int rows = 7;
-    // int cols = 7;
-    // int the_map_temp[rows][cols] = {
-    //     {0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 1, 1, 1, 0, 0},
-    //     {0, 0, 0, 0, 1, 0, 0},
-    //     {0, 0, 1, 1, 1, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0},
-    // };
-    // position start_position = {0, 0};
-    // position target_position = {6, 6};
-
-    // 地图构建2
-    int rows = 7;
-    int cols = 7;
+    // 地图构建
+    int rows = 9;
+    int cols = 15;
     int the_map_temp[rows][cols] = {
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0},
-        {0, 0, 1, 1, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     };
-    position start_position = {3, 1};
-    position target_position = {3, 5};
-
-    // // 地图构建3
-    // int rows = 7;
-    // int cols = 7;
-    // int the_map_temp[rows][cols] = {
-    //     {0, 0, 0, 0, 0, 1, 0},
-    //     {0, 1, 1, 1, 1, 1, 0},
-    //     {0, 1, 0, 0, 0, 1, 0},
-    //     {0, 1, 0, 1, 0, 1, 0},
-    //     {0, 1, 0, 1, 0, 1, 0},
-    //     {0, 1, 0, 1, 0, 1, 0},
-    //     {0, 0, 0, 1, 0, 0, 0},
-    // };
-    // position start_position = {0, 0};   // 左上角
-    // position target_position = {0, 6};  // 右上角
+    position start_position = {4, 2};
+    position target_position = {3, 12};
 
 
     int* the_map[rows];
@@ -367,8 +395,12 @@ void run(){
     
     // 展示路径
     printf("Start: (%d,%d)\n  End: (%d,%d)\n", start_position.y, start_position.x, target_position.y, target_position.x);
-    print_n();
+    // print_n();
     show_route(&lines);
+    
+    printf("\n\n\n");
+
+    draw_result(the_map, start_position, target_position, &lines, rows, cols);
 }
 
 
