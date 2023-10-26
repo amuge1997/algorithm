a767f80e1e467003abb28914feed803d71ea67b9
“amuge1997” <amuge1997z@qq.com>
2023-10-27 06:29:47 +0800

    算法

--- 神经网络/nn.cpp
+++ 神经网络/nn.cpp
@@ -20,8 +20,8 @@ class NN{
         this->lr = lr;                      // 学习率
         this->epochs = epochs;              // 轮次
         this->batch = batch;                // 批数量
-        this->weights1.resize(hidden_dims, x_dims+1);   // 权重矩阵1
-        this->weights2.resize(y_dims, hidden_dims+1);   // 权重矩阵2
+        this->weights1.resize(hidden_dims, x_dims+1);   // 参数矩阵1
+        this->weights2.resize(y_dims, hidden_dims+1);   // 参数矩阵2
     }
 
     MatrixXf forward(const MatrixXf& x){
