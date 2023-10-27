4b054d67e949d2ecf6a8ff7c248113eb019fedb5
“amuge1997” <amuge1997z@qq.com>
2023-10-28 02:30:55 +0800

    算法

--- 神经网络c++/nn.cpp
+++ 神经网络c++/nn.cpp
@@ -9,10 +9,9 @@ using namespace std;
 using namespace Eigen;
 
 
-// 网络
+// nn网络
 class NN{
     public:
-    
     NN(int x_dims, int y_dims, int hidden_dims, float lr, int epochs, int batch){
         this->x_dims = x_dims;              // 输入维度
         this->y_dims = y_dims;              // 输出维度
@@ -20,8 +19,9 @@ class NN{
         this->lr = lr;                      // 学习率
         this->epochs = epochs;              // 轮次
         this->batch = batch;                // 批数量
-        this->weights1.resize(hidden_dims, x_dims+1);   // 参数矩阵1
-        this->weights2.resize(y_dims, hidden_dims+1);   // 参数矩阵2
+        this->weights1.resize(hidden_dims, x_dims+1);       // 参数矩阵1
+        this->weights2.resize(hidden_dims, hidden_dims+1);  // 参数矩阵2
+        this->weights3.resize(y_dims, hidden_dims+1);       // 参数矩阵2
     }
 
     // 预测
@@ -29,20 +29,28 @@ class NN{
         int nums = x.cols();
         
         MatrixXf bias(1, nums);                         // 偏置向量
-        MatrixXf x_bias(x_dims + 1, nums);               // 添加偏置后的输入矩阵
-        MatrixXf hidden_sig_bias(hidden_dims+1, nums);   // 添加偏置后的中间矩阵
+        MatrixXf x_bias(x_dims + 1, nums);              // 添加偏置后的输入矩阵
+        MatrixXf hidden1_sig_bias(hidden_dims+1, nums); // 添加偏置后的中间矩阵
+        MatrixXf hidden2_sig_bias(hidden_dims+1, nums); // 添加偏置后的中间矩阵
 
-        MatrixXf hidden;
-        MatrixXf hidden_sig;
+        MatrixXf hidden1;
+        MatrixXf hidden2;
+        MatrixXf hidden1_sig;
+        MatrixXf hidden2_sig;
         MatrixXf out;
         
         bias.setOnes();
         x_bias << x, bias;
-        hidden = weights1 * x_bias;
-        hidden_sig = 1.0 / (1.0 + (-hidden.array()).exp());
-        hidden_sig_bias << hidden_sig, bias;
+
+        hidden1 = weights1 * x_bias;
+        hidden1_sig = 1.0 / (1.0 + (-hidden1.array()).exp());
+        hidden1_sig_bias << hidden1_sig, bias;
+
+        hidden2 = weights2 * hidden1_sig_bias;
+        hidden2_sig = 1.0 / (1.0 + (-hidden2.array()).exp());
+        hidden2_sig_bias << hidden2_sig, bias;
         
-        out = weights2 * hidden_sig_bias;
+        out = weights3 * hidden2_sig_bias;
         return out;
     }
 
@@ -53,23 +61,36 @@ class NN{
         MatrixXf bias(1, batch);            // 偏置向量
         MatrixXf x(x_dims, batch);          // 输入层
         MatrixXf y(y_dims, batch);          // 标签
-        MatrixXf x_bias(x_dims + 1, batch);  // 输入偏置层
-        MatrixXf hidden_sig_bias(hidden_dims+1, batch);  // 中间偏置层
+        MatrixXf x_bias(x_dims + 1, batch); // 输入偏置层
+        MatrixXf hidden1_sig_bias(hidden_dims+1, batch);    // 中间偏置层
+        MatrixXf hidden2_sig_bias(hidden_dims+1, batch);    // 中间偏置层
 
-        MatrixXf hidden;                // 中间层
-        MatrixXf hidden_sig;            // sigmoid激活层
-        MatrixXf out;                   // 输出层
-        MatrixXf error;                 // 误差
-        float error2;                   // 总误差
-        MatrixXf dEdW2;                 // 误差对W2梯度矩阵
-        MatrixXf dEdHsig;
-        MatrixXf dEdHone;
-        MatrixXf dEdH;                  // 误差对中间层梯度矩阵
-        MatrixXf dEdW1;                 // 误差对W1梯度矩阵
+        MatrixXf hidden1;                   // 隐藏层1
+        MatrixXf hidden2;                   // 隐藏层1
+        MatrixXf hidden1_sig;               // sigmoid激活层1
+        MatrixXf hidden2_sig;               // sigmoid激活层2
+        MatrixXf out;                       // 输出层
+        MatrixXf error;                     // 误差
+        float error2;                       // 总误差
+
+        MatrixXf dEdO;                      // 误差对输出梯度
+        MatrixXf dEdW3;                     // 误差对W3梯度
+
+        MatrixXf dEdH2sig;                  // 误差对sigmoid激活层2梯度
+        MatrixXf dEdH2bias;                 // 误差对含偏置隐藏层2梯度
+        MatrixXf dEdH2;                     // 误差对隐藏层2梯度
+        MatrixXf dEdW2;                     // 误差对W2梯度
+
+        MatrixXf dEdH1sig;                  // 误差对sigmoid激活层1梯度
+        MatrixXf dEdH1bias;                 // 误差对含偏置隐藏层1梯度
+        MatrixXf dEdH1;                     // 误差对隐藏层1梯度
+        MatrixXf dEdW1;                     // 误差对W1梯度
         
         // 初始化参数矩阵
         weights1 = MatrixXf::Random(hidden_dims, x_dims+1).array() / sqrt(hidden_dims + x_dims+1);
-        weights2 = MatrixXf::Random(y_dims, hidden_dims+1).array() / sqrt(y_dims + hidden_dims+1);
+        weights2 = MatrixXf::Random(hidden_dims, hidden_dims+1).array() / sqrt(hidden_dims + hidden_dims+1);
+        weights3 = MatrixXf::Random(y_dims, hidden_dims+1).array() / sqrt(y_dims + hidden_dims+1);
+
         // 偏置向量
         bias.setOnes();
         
@@ -80,39 +101,62 @@ class NN{
             numbers.push_back(i);
         }
         for(int i=0;i<epochs;i++){
-            // 随机抽样
+            // 每批次随机抽样
             random_shuffle(numbers.begin(), numbers.end());
-            for (int i = 0; i < batch; i++) {
-                x.col(i) = x_ori.col(numbers[i]);
-                y.col(i) = y_ori.col(numbers[i]);
+            int batch_sum = nums / batch;
+            for(int j=0;j<batch_sum;j++){
+                for (int bi = 0; bi < batch; bi++){
+                    x.col(bi) = x_ori.col(numbers[bi + j * batch]);
+                    y.col(bi) = y_ori.col(numbers[bi + j * batch]);
+                }
+                
+                // 添加偏置
+                x_bias << x, bias;
+                
+                // h1 = sigmoid( w1 * x )    使用sigmoid激活
+                hidden1 = weights1 * x_bias;
+                hidden1_sig = 1.0 / (1.0 + (-hidden1.array()).exp());
+                hidden1_sig_bias << hidden1_sig, bias;
+
+                // h2 = sigmoid( w2 * h1 )    使用sigmoid激活
+                hidden2 = weights2 * hidden1_sig_bias;
+                hidden2_sig = 1.0 / (1.0 + (-hidden2.array()).exp());
+                hidden2_sig_bias << hidden2_sig, bias;
+                
+                // 为了能全域映射,不进行激活直接线性映射得到输出
+                // o = w3 * h2
+                out = weights3 * hidden2_sig_bias;
+
+                // 误差
+                // e = (Y - O)^2
+                error = y - out;
+                error2 = error.array().square().sum() / batch;
+                
+                // 链式法则求各矩阵梯度
+                // 输出层梯度
+                dEdO = -2./batch * error.array();
+                dEdW3 = dEdO * hidden2_sig_bias.transpose();
+                dEdH2sig = weights3.transpose() * dEdO;
+
+                // 第二层梯度
+                dEdH2bias = dEdH2sig.array() * hidden2_sig_bias.array() * (1.0 - hidden2_sig_bias.array());
+                dEdH2 = dEdH2bias.topRows(dEdH2bias.rows()-1);
+                dEdW2 = dEdH2 * hidden1_sig_bias.transpose();
+                dEdH1sig = weights2.transpose() * dEdH2;
+
+                // 第一层梯度
+                dEdH1bias = dEdH1sig.array() * hidden1_sig_bias.array() * (1.0 - hidden1_sig_bias.array());
+                dEdH1 = dEdH1bias.topRows(dEdH1bias.rows()-1);
+                dEdW1 = dEdH1 * x_bias.transpose();
+
+                // 更新参数矩阵
+                weights1 = weights1 - lr * dEdW1;
+                weights2 = weights2 - lr * dEdW2;
+                weights3 = weights3 - lr * dEdW3;
             }
-            // 添加偏置
-            x_bias << x, bias;
-            
-            // h = sigmoid( w1 * x )    使用sigmoid激活
-            hidden = weights1 * x_bias;
-            hidden_sig = 1.0 / (1.0 + (-hidden.array()).exp());
-            hidden_sig_bias << hidden_sig, bias;
-
-            // 为了能全域映射,不进行激活直接线性映射得到输出. 由于输出层不进行激活,因此该网络的非线性能力不强
-            // o = w2 * h               
-            out = weights2 * hidden_sig_bias;
-
-            // 误差
-            // e = (Y - O)^2
-            error = y - out;
-            error2 = error.array().square().sum();
-            
-            // 链式法则求各矩阵梯度
-            dEdW2 = -2. * (error * hidden_sig_bias.transpose()).array();
-            dEdHsig = -2. * (weights2.transpose() * error).array();
-            dEdHone = hidden_sig_bias.array() * (1.0 - hidden_sig_bias.array());
-            dEdH = dEdHone.topRows(dEdHone.rows()-1);
-            dEdW1 = dEdH * x_bias.transpose();
-            
-            // 更新参数矩阵
-            weights1 = weights1 - lr * dEdW1;
-            weights2 = weights2 - lr * dEdW2;
+            if(i % 500 == 0){
+                printf("%d/%d error %f\n", i, epochs, error2);
+            }
         }
     }
 
@@ -125,31 +169,36 @@ class NN{
     int batch;
     MatrixXf weights1;
     MatrixXf weights2;
+    MatrixXf weights3;
 };
 
 
 int main(){
-
+    // 读取训练样本
     MatrixXf xt = readx("x.txt").transpose();
     MatrixXf yt = ready("y.txt").transpose();
-    
+    // 读取测试样本
     MatrixXf x_mesh = readx("x_mesh.txt").transpose();
 
+    // nn参数
     int x_dims = xt.rows();
     int y_dims = yt.rows();
-    int hidden_nums = 8;
-    float lr = 1e-3;
-    int epochs = 1000;
-    int batch = 8;
+    int hidden_nums = 32;
+    float lr = 1e-1;
+    int epochs = 10000;
+    int batch = 20;
 
+    // nn实例化
     NN nn(x_dims, y_dims, hidden_nums, lr, epochs, batch);
     nn.train(xt, yt);
     
+    // 训练样本预测
     MatrixXf p1 = nn.predict(xt);
+    // 测试样本预测
     MatrixXf predict = nn.predict(x_mesh);
 
+    // 保存测试预测结果
     savey(predict.transpose(), "y_mesh.txt");
-
     return 0;
 }
 
